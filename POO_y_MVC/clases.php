6c12ada6905d3c2a357dba9faadc1db068d797a8
jcdavidarg <jcdavidarg@gmail.com>
2024-05-05 22:03:16 -0300

    poo and mvc theory

---
+++ POO_y_MVC/clases.php
@@ -0,0 +1,33 @@
+<?php
+// Declarar una clase
+class NombreDeMiClase {
+    // Propiedades, atributos y funciones
+}
+
+// Declarar una clase abstractica
+abstract class ClaseAbstractica {
+    // Propiedades, atributos y funciones
+}
+
+// Herencia de clases
+
+// Clase madre
+class MiClase {
+    //...
+}
+
+// Clase hija
+class HijaDeMiClase extends MiClase {
+    // Esta clase hereda todos los metodos y propiedades de la clase madre
+}
+
+// Clase final
+final class MiClaseFinal {
+    // No podra heredar
+}
+
+
+//-------------------------------
+
+
+?>
---
+++ POO_y_MVC/instancias.php
@@ -0,0 +1,19 @@
+<?php
+
+//include './clases.php';
+
+class Persona {
+    public $nombre;
+    public $genero;
+    private $edad;
+    protected $pasaporte;
+}
+
+/* $obj_one = new Persona;
+$obj_one->nombre = 'casa';
+
+echo 'funciona' . '<br/>' . $obj_one->nombre . '<br/>';
+print_r($obj_one); */
+
+
+?>
